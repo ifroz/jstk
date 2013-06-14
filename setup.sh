cf56b30c0ef667155c9a385cb322a78ba3a15f2d
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2013-06-14 19:10:37 -0400

    setup script fixed 2

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git clone https://code.google.com/p/closure-library/ lib/closure-library
 
-mkdir externs -p
-pushd /lib/externs
+mkdir lib/externs -p
+pushd lib/externs
 wget http://closure-compiler.googlecode.com/git/contrib/externs/jasmine.js
 wget http://closure-compiler.googlecode.com/git/contrib/externs/underscore-1.3.1.js
 wget http://closure-compiler.googlecode.com/git/contrib/externs/jquery-1.9.js
