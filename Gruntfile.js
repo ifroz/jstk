b76e5d3072aec5334b79df96ac90a3b708179427
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2014-01-21 16:17:58 +0100

    changed to lodash

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,116 +1,22 @@
 'use strict';
 
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-closure-tools');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-
-  var defaultCompilerOpts = {
-    externs: [
-      'src/externs/*.js',
-      'lib/externs/*.js'
-    ],
-    source_map_format: 'V3',
-    language_in: 'ECMASCRIPT5_STRICT',
-    output_wrapper: '"%output% //@ sourceMappingURL=/helpers.js.map"',
-    closure_entry_point: 'jstk.test',
-    create_source_map: 'dist/helpers.js.map',
-    warning_level: 'verbose',
-    jscomp_warning: [
-      'checkTypes',
-      'visibility',
-      'accessControls',
-      'checkDebuggerStatement',
-      'checkRegExp',
-      'const',
-      'constantProperty',
-      'deprecated',
-      'duplicate',
-      'invalidCasts',
-      'strictModuleDepCheck',
-      'suspiciousCode',
-      'missingProperties',
-      'uselessCode',
-      'undefinedVars'
-    ],
-    compilation_level: 'SIMPLE_OPTIMIZATIONS'
-  };
-
-  var nodeWrapper =
-      '"var _ = require(\'underscore\');\n' +
-      '%output%\n' +
-      'module.exports.jstk = jstk;\n' +
-      'module.exports.underscore = _;"';
-
-  var distCompilerOpts = {
-    output_wrapper: nodeWrapper,
-    externs: [
-      'src/externs/*.js',
-      'lib/externs/*.js'
-    ],
-    language_in: 'ECMASCRIPT5_STRICT',
-    closure_entry_point: 'jstk.lib',
-    compilation_level: 'SIMPLE_OPTIMIZATIONS'
-  };
-
-  var jsSource = [
-    'src/*.js',
-    'lib/closure-library/closure/goog/base.js'
-  ];
-
-  var jsTest = [
-    'test/src/helper/*.js',
-    'test/src/*.js',
-    'test/src/unit/*.js'
-  ];
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    closureCompiler: {
+    jshint: {
       options: {
-        compilerFile: '/opt/closure-compiler/compiler.jar'
+        jshintrc: true
       },
-      buildTest: {
-        TEMPcompilerOpts: defaultCompilerOpts,
-        src: jsSource.concat(jsTest),
-        dest: 'build/all.js'
-      },
-      buildNode: {
-        TEMPcompilerOpts: distCompilerOpts,
-        src: jsSource,
-        dest: 'dist/jstk.js'
-      }
-    },
-    copy: {
-      libJS: {
-        files: [
-          {
-            src: 'build/all.js',
-            dest: 'test/bin/test.js'
-          }
-        ]
-      }
-    },
-    watch: {
-      allJS: {
-        files: jsSource,
-        tasks: [
-          'build'
-        ],
-        options: {
-          nospawn: true,
-          forever: false
-        }
-      }
+      all: [
+        'Gruntfile.js',
+        'lib/**/*.js',
+        'test/**/*.js'//,
+       // 'features/**/*.js'
+      ]
     }
   });
-
-  grunt.registerTask('buildTest', [
-    'closureCompiler:buildTest',
-    'copy:libJS'
-  ]);
-
-  grunt.registerTask('default', [
-    'build'
-  ]);
+  
+  grunt.registerTask('test', ['jshint']);
 };
+
