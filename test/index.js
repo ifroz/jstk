dcaaef0f58057e36dda5d0077a22d650bb4ed7bf
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2014-01-23 16:18:03 +0100

    bind jstk to lodash tests

---
+++ test/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var _ = require('lodash');
+var jstk = require('../lib/index.js');
+var expect = require('expect.js');
+
+describe('helper', function() {
+  it('should have a bind method', function() {
+    expect(jstk.bind).to.be.a(Function);
+  });
+
+  it('should bind jstk methods to lodash', function() {
+    jstk.bind();
+    expect(_.isObjectStrict).to.be.a(Function);
+    expect(_.applyCtor).to.be.a(Function);
+    expect(_.callCtor).to.be.a(Function);
+    expect(_.toInt).to.be.a(Function);
+    expect(_.matcher).to.be.a(Function);
+  });
+});
