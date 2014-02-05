57c2d6b1e6ce34299083e068db86819eee0c6381
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2014-02-05 15:13:53 +0100

    grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,30 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  var jsFiles = [
+    'Gruntfile.js',
+    'lib/**/*.js',
+    'test/**/*.js'
+  ];
 
   grunt.initConfig({
     jshint: {
       options: {
         jshintrc: true
       },
-      all: [
-        'Gruntfile.js',
-        'lib/**/*.js',
-        'test/**/*.js'
-      ]
+      all: jsFiles
     },
     exec: {
       mocha: {
         command: 'mocha'
+      }
+    },
+    watch: {
+      test: {
+        files: jsFiles,
+        tasks: ['test']
       }
     }
   });
